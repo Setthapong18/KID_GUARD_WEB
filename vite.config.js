f50d893d267577f8ce5fd7f3e80934f9fcb7676d
Luvchakhiao <109526309+Mynnts@users.noreply.github.com>
2026-03-03 20:26:10 +0700

    Update vite config for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/KidGuardWEB/',
+  base: '/KID_GUARD_WEB/',
   plugins: [react(), tailwindcss()],
 })
